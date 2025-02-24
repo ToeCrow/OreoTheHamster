fb8022dcbfacf10f94d95f3675f5a400e7a055b9
ToeCrow <thomas.kronvold@chasacademy.se>
2025-02-24 16:25:07 +0100

    first page started

---
+++ src/Pages/Start.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Start = () => {
+  return (
+    <div>
+      <button>Bilder</button>
+      <button>Hamstervård</button>
+    </div>
+  )
+}
+
+export default Start
